7e99254a3e1cb22f2248f82c1237805ffdcbc748
Vitalii Antonov <Vitalii_Antonov@epam.com>
2020-11-08 13:04:51 +0400

    Add new product creation

--- product-service/data/repositories/products-repository-impl.ts
+++ product-service/data/repositories/products-repository-impl.ts
@@ -20,4 +20,30 @@ export class ProductsRepositoryImpl implements ProductsRepository {
     `, [id]);
     return result.rows ? result.rows[0] : null;
   }
+
+  async create(data: Product): Promise<Product | null> {
+    const {title, description, price, count} = data;
+    let result;
+
+    try {
+      await this.db.query('BEGIN');
+      result = await this.db.query(
+        `INSERT INTO products(title, description, price) VALUES($1, $2, $3) RETURNING *`,
+        [
+          title,
+          description,
+          price,
+        ]
+      );
+      const product = result.rows[0] || {};
+      await this.db.query(`INSERT INTO stocks(product_id, count) VALUES($1, $2)`, [product.id, count]);
+      await this.db.query('COMMIT')
+
+      return {...product, count};
+
+    } catch (error) {
+      await this.db.query('ROLLBACK')
+      throw error;
+    }
+  }
 }
--- product-service/interfaces/products-repository.ts
+++ product-service/interfaces/products-repository.ts
@@ -3,4 +3,5 @@ import {Product} from "./product";
 export interface ProductsRepository {
   findAll(): Promise<Product[]>;
   findById(id: string): Promise<Product>;
+  create(data: Omit<Product, 'id'>): Promise<Product>;
 }
